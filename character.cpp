66963be60d019d0fb533f236a20f7326818a6979
alstn4676 <meatboog@naver.com>
2020-07-28 16:06:27 +0900

    2020-07-28
    character

---
+++ character.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <iostream>
+#include <cmath>
+#include "Constant.h"
+#include "Map.cpp"
+#include "Resolution.h"
+
+class Character
+{
+private:
+	float x, y;
+	int Heart;
+	int Shield;
+	float MoveSpeed;
+	float JumpPower;
+	float AttackSpeed;
+	int Weapon;
+public:
+
+};
